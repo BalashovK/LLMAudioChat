9f4b7025753a2c899c56d80bf16b43b14eef3ee5
BalashovK <xcotty@hotmail.com>
2023-10-22 10:56:33 -0700

    First version

---
+++ SpeechToText.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Whisper.net;
+using Whisper.net.Ggml;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace LLMAudioChat
+{
+    public class SpeechToText : IDisposable
+    {
+        public GgmlType ModelType { get; set; } = GgmlType.Base;
+
+        public string last_string = "";
+        WhisperProcessor processor;
+        WhisperFactory factory;
+        public string ModelName { get; set; } = "en-us-base.ggml";  
+
+        public SpeechToText(string a_modelName)
+        {
+            ModelName = a_modelName;
+            if (!File.Exists(ModelName))
+            {
+                Console.WriteLine($"Downloading Model {ModelName}");
+                using var modelStream = WhisperGgmlDownloader.GetGgmlModelAsync(ModelType).Result;
+                using var fileWriter = File.OpenWrite(ModelName);
+                modelStream.CopyToAsync(fileWriter);
+            }
+
+            // Same factory can be used by multiple task to create processors.
+            factory = WhisperFactory.FromPath(ModelName);
+
+            WhisperProcessorBuilder builder = factory.CreateBuilder()
+                .WithLanguage("en");
+
+            processor = builder.Build();
+        }
+
+        public async Task<string> S2T(Stream stream)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+//            stream.Position = 0;
+
+//            string fn = @"D:\Whisper\With_NAudio\Test\test_chat_test.wav";
+//            string fn= @"D:\Whisper\With_NAudio\output.wav";
+//            FileStream fileStream = File.OpenRead(fn);
+
+            await foreach (var segment in processor.ProcessAsync(stream, CancellationToken.None))
+            {
+                Console.WriteLine($"New Segment: {segment.Start} ==> {segment.End} : {segment.Text}");
+                stringBuilder.Append(segment.Text + " ");
+            }
+            last_string = stringBuilder.ToString();
+            return last_string;
+        }
+        public void Dispose()
+        {
+            processor?.Dispose();
+            factory?.Dispose();
+        }
+    }
+}
---
+++ TextToSpeech.cs
@@ -0,0 +1,109 @@
+﻿using NAudio.Dmo.Effect;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Speech.Synthesis;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LLMAudioChat
+{
+    public class TextToSpeech
+    {
+        StringBuilder sb = new StringBuilder();
+        SpeechSynthesizer synthesizer;
+        Queue<string> queue = new Queue<string>();
+
+        CancellationToken m_ct;
+
+         public TextToSpeech()
+         {
+            Initialize();
+         }
+        public void Initialize()
+        {
+            synthesizer = new SpeechSynthesizer();
+            synthesizer.SetOutputToDefaultAudioDevice();
+            synthesizer.Rate = 1;
+
+            Task.Run(async () => await SpeakTask());
+        }
+        public void Speak(IEnumerable<string> text_stream, CancellationToken ct)
+        {
+            m_ct = ct;
+            string last_chunk = "";
+            foreach (string s in text_stream)
+            {
+                if (ct.IsCancellationRequested)
+                {
+                    Console.WriteLine("<<<Cancellation requested>>>");
+                    queue.Clear();
+                    sb.Clear();
+                    return;
+                }
+
+                bool last_chunk_was_punctuation = (last_chunk.EndsWith('.') || last_chunk.EndsWith('!') || last_chunk.EndsWith('?') || last_chunk.EndsWith(';'));
+                bool this_chunk_is_space = (s.StartsWith(' ') || s.StartsWith('\n') || s.StartsWith('\r') || s.StartsWith('\t'));
+
+                bool split = (s==" and" || s==" or" || s==" but" || s==" so" || (last_chunk_was_punctuation && this_chunk_is_space));
+                if (!split)
+                {
+                    sb.Append(s);
+
+                    Console.Write(s);
+                }
+                else
+                {
+                    string text_to_speak = sb.ToString();
+                    queue.Enqueue(text_to_speak);
+                    sb.Clear();
+                    sb.Append(s);
+                    Console.Write(s);
+                }
+
+                last_chunk = s;
+            }
+            queue.Enqueue(sb.ToString());
+            sb.Clear();
+        }
+        private async Task SpeakTask()
+        {
+            while (true)
+            {
+                if(queue.Count > 0)
+                {
+                    StringBuilder sb2 = new StringBuilder();
+                    while (queue.Count > 0)
+                    {
+                        sb2.Append(queue.Dequeue());
+                    }
+                    string string_to_speek = sb2.ToString();
+                    if (string_to_speek.ToLower().EndsWith("user:"))
+                    {
+                        string_to_speek = string_to_speek.Substring(0, string_to_speek.Length - 5);
+                    }
+
+                    Prompt p = synthesizer.SpeakAsync(string_to_speek);
+
+                    // wait for completion
+                    while (!p.IsCompleted)
+                    {
+                        if (m_ct.IsCancellationRequested)
+                        {
+//                            Console.WriteLine("<<<Cancellation requested>>>");
+                            synthesizer.SpeakAsyncCancelAll();
+                            queue.Clear();
+                            break;
+                        }
+                        await Task.Delay(10);
+                    }
+                    
+                }
+                else
+                {
+                    await Task.Delay(10);
+                }
+            }
+        }
+    }
+}
